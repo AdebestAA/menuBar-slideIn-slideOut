aad02c3bf19ffd921fe17b10dcadf94c71bb7560
Adebest <aadebesta@gmail.com>
2024-05-20 12:42:55 +0100

    add project

---
+++ index.js
@@ -0,0 +1,22 @@
+let menuButton = document.querySelector('.third-section')
+
+
+let cancel = document.querySelector('.cancel')
+
+let ul = document.querySelector('.container ul')
+
+let btnContainer = document.querySelector('.btn-container')
+console.log(btnContainer);
+menuButton.addEventListener('click', () => {
+
+
+    menuButton.classList.toggle('section-three')
+
+})
+
+
+
+
+
+
+
